6596ea73d54e88afbf6c8f6f0f0586880388076f
finleigh <finleighgoyette@Finleighs-MacBook-Pro.local>
2019-04-17 19:55:21 -0400

    ddd

--- js/main.js
+++ js/main.js
@@ -1,65 +1,86 @@
 
-let alldata= [
-  {
-    day: {
-      month:`Jan`,
-      date: `30`,
-    },
-    sleep: `8`,
-    water: `26`,
-    steps:`7430`,
-    active:`1.5`,
-    weight: `154`
-  },
-  {
-    day: {
-      month:`Jan`,
-      date: `31`,
-    },
-    sleep: `6.5`,
-    water: `38`,
-    steps:`4987`,
-    active:`30`,
-    weight: `154`
-  },
-  {
-    day: {
-      month:`Feb`,
-      date: `1`,
-    },
-    sleep: `7`,
-    water: `30`,
-    steps:`5003`,
-    active:`30`,
-    weight: `154`
-  },
-  {
-    day: {
-      month:`Feb`,
-      date: `2`,
-    },
-    sleep: `9.5`,
-    water: `44`,
-    steps:`13884`,
-    active:`1.5`,
-    weight: `154`
-  }
-]
-
-alldata.forEach( rtrack => {
 
 
-document.getElementById('righttrack').innerHTML +=
-    `<li class="user">
-      <div>${rtrack.day.month}-${rtrack.day.date}</div>
-      <div>${rtrack.sleep} Hrs</div>
-      <div>${rtrack.weight} Hrs</div>
-      <div>${rtrack.water} Ozs</div>
-      <div>${rtrack.steps} Steps</div>
-      <div>${rtrack.active} Hrs</div>
-     </li>`;
 
-} );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let alldata= [
+//   {
+//     day: {
+//       month:`Jan`,
+//       date: `30`,
+//     },
+//     sleep: `8`,
+//     water: `26`,
+//     steps:`7430`,
+//     active:`1.5`,
+//     weight: `154`
+//   },
+//   {
+//     day: {
+//       month:`Jan`,
+//       date: `31`,
+//     },
+//     sleep: `6.5`,
+//     water: `38`,
+//     steps:`4987`,
+//     active:`30`,
+//     weight: `154`
+//   },
+//   {
+//     day: {
+//       month:`Feb`,
+//       date: `1`,
+//     },
+//     sleep: `7`,
+//     water: `30`,
+//     steps:`5003`,
+//     active:`30`,
+//     weight: `154`
+//   },
+//   {
+//     day: {
+//       month:`Feb`,
+//       date: `2`,
+//     },
+//     sleep: `9.5`,
+//     water: `44`,
+//     steps:`13884`,
+//     active:`1.5`,
+//     weight: `154`
+//   }
+// ]
+//
+// alldata.forEach( rtrack => {
+//
+//
+// document.getElementById('righttrack').innerHTML +=
+//     `<li class="user">
+//       <div>${rtrack.day.month}-${rtrack.day.date}</div>
+//       <div>${rtrack.sleep} Hrs</div>
+//       <div>${rtrack.weight} Hrs</div>
+//       <div>${rtrack.water} Ozs</div>
+//       <div>${rtrack.steps} Steps</div>
+//       <div>${rtrack.active} Hrs</div>
+//      </li>`;
+//
+// } );
 
 // let weightdata= [
 //   {
